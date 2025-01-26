51918e868f6eb8fde2426dd241f45c4862b374b5
Oleksii Ivaniuta <oivaniuta@gmail.com>
2025-01-26 16:40:46 +0000

    hw3 ready

---
+++ js/task-2.js
@@ -0,0 +1,21 @@
+const firstArray = null;
+const secondArray = null;
+const maxLength = null;
+function makeArray(firstArray, secondArray, maxLength) {
+    const newArray = firstArray.concat(secondArray);
+    if (maxLength <= newArray.length) {
+        const newArrayCopy = newArray.slice(0, maxLength);
+        return (newArrayCopy);
+    }
+    else {
+        
+        return (newArray);
+    }
+}
+
+console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3)); // ["Mango", "Poly", "Ajax"]
+console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
+console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3)); // ["Mango", "Ajax", "Chelsea"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2)); // ["Earth", "Jupiter"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0)); // []
